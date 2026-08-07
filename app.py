d50e2b731637b43e7bb0c951032b610edcafb409
Tanmay Dhodad <tanmaydhodad4545.com>
2026-08-07 15:05:37 +0530

    Update medical shop management system

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
+
 @app.route("/medicines")
 def medicines():
-    return render_template("products.html")
+    return render_template("medicines.html")
 
 
 @app.route("/customers")
@@ -15,3 +21,7 @@ def customers():
 @app.route("/billing")
 def billing():
     return render_template("billing.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
